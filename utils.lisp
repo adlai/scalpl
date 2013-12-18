6e11df7da16a048bd73bcc896a408598830fbbc4
Adlai Chandrasekhar <munchking@gmail.com>
2013-12-19 01:50:15 +0200

    Useful macro WITH-JSON-SLOTS

--- utils.lisp
+++ utils.lisp
@@ -24,3 +24,11 @@
 (defun currency-pair (c1 c2)
   "c1 and c2 should be three-letter keywords such as :btc or :usd"
   (format nil "~A~A" c1 c2))
+
+(defmacro with-json-slots (slot-vars object &body body)
+  `(with-slots
+         ,(mapcar (lambda (slot)
+                    `(,slot ,(intern (symbol-name slot) :keyword)))
+                  slot-vars)
+       ,object
+     ,@body))
