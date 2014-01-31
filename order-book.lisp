48a048217a6a07524ec1aa93d31ce1abef544501
Adlai Chandrasekhar <munchking@gmail.com>
2014-01-31 10:43:53 +0200

    Neater order book

--- order-book.lisp
+++ order-book.lisp
@@ -1,7 +1,8 @@
 ;;;; utils.lisp
 
 (defpackage #:glock.order-book
-  (:use #:cl #:glock.util #:glock.connection #:st-json))
+  (:use #:cl #:glock.util #:glock.connection #:st-json)
+  (:export #:order-book))
 
 (in-package #:glock.order-book)
 
@@ -14,10 +15,10 @@
   output)
 
 (defun depth-message-price (jso)
-  (/ (parse-integer (getjso* "depth.price_int" jso)) (expt 10 5)))
+  (/ (parse-integer (getjso* "depth.price_int" jso)) #.(expt 10 5)))
 
 (defun depth-message-volume (jso)
-  (/ (parse-integer (getjso* "depth.volume_int" jso)) (expt 10 8)))
+  (/ (parse-integer (getjso* "depth.volume_int" jso)) #.(expt 10 8)))
 
 ;;; Talking to the API
 
@@ -27,55 +28,20 @@
 (defmethod initialize-instance :around ((book order-book) &key full)
   (call-next-method book :path (format nil "money/depth/~:[fetch~;full~]" full)))
 
+(defstruct (order (:constructor %make-order)) price amount time)
+
+(defun make-order (raw-order)
+  (with-json-slots (stamp price_int amount_int) raw-order
+    (multiple-value-bind (sec nsec) (floor (parse-integer stamp) #.(expt 10 6))
+      (%make-order :time (local-time:unix-to-timestamp sec :nsec nsec)
+                   :price (/ (parse-integer price_int) #.(expt 10 5))
+                   :amount (/ (parse-integer amount_int) #.(expt 10 8))))))
+
 (defmethod request :around ((connection mtgox-connection) (method order-book))
-  (let ((book (call-next-method)))
-    (setf (getjso "bids" book)
-          (reverse (getjso "bids" book)))
-    book))
-
-;;; Helper structs
-(defstruct (order (:constructor nil)) price amount time)
-(defstruct (bid (:include order)))
-(defstruct (ask (:include order)))
-
-(defun make-order (type raw-order)
-  (with-json-slots (price--int amount--int stamp) raw-order
-    (funcall (ccase type
-               (bid #'make-bid)
-               (ask #'make-ask))
-             :amount (parse-integer amount--int)
-             :price (parse-integer price--int)
-             :time (parse-integer stamp))))
-
-;;; Datastructure to store order book info
-
-;;; How the order book itself should look:
-;;; Two alists, one of bids, one of asks
-;;; Each one is a list of ( price-int . amount-int )
-;;; where amount-int is the total of all orders at that price
-
-;; (defclass order-book ()
-;;   ((full :initarg :full)
-;;    currency-pair min-price max-price bids asks timestamp))
-
-;; (macrolet ((define-order-adder (name accessor comparison)
-;;              `(defun ,name (order book)
-;;                 (if (,accessor book))))))
-
-;; (defmethod initialize-instance ((book order-book) &key full (c1 :btc) (c2 :usd) pair)
-;;   (with-slots (currency-pair data min-price max-price timestamp) book
-;;     (setf currency-pair (or pair (currency-pair c1 c2)))
-;;     (with-json-slots (bids asks now cached)
-;;         (get-order-book :full full :pair currency-pair)
-;;       (flet ((price-int (order) (parse-integer (slot-value order :price--int))))
-;;         (setf max-price (reduce #'max asks :key #'price-int)
-;;               min-price (reduce #'min bids :key #'price-int)
-;;               timestamp (if full now cached)
-;;               data (make-hash-table))
-;;         (flet ((add-orders (orders type)
-;;                  (loop
-;;                     for order across orders
-;;                     do (push (make-order type order)
-;;                              (gethash (price-int order) data)))))
-;;           (add-orders bids 'bid)
-;;           (add-orders asks 'ask))))))
+  (with-json-slots (now bids asks) (call-next-method)
+    (let ((asks (mapcar #'make-order asks))
+          (bids (reduce (lambda (bids order) (cons (make-order order) bids))
+                        bids :initial-value nil)))
+      (jso "now" now
+           "asks" (remove 1/100000000 asks :key #'order-amount)
+           "bids" (remove 1/100000000 bids :key #'order-amount)))))
