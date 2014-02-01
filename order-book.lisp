ca450432abe09e81bde3b12657930474838cef97
Adlai Chandrasekhar <munchking@gmail.com>
2014-02-02 00:25:15 +0200

    Fixed apply-depth-message

--- order-book.lisp
+++ order-book.lisp
@@ -20,7 +20,7 @@
     (jso "now" (goxstamp (parse-integer now))
          "type" (concatenate 'string type_str "s")
          "price" (/ (parse-integer price_int) (expt 10 5))
-         "delta" (/ (parse-integer volume_int) (expt 10 8))
+         "delta" (/ (parse-integer volume_int) (expt 10 8)) ; TODO: Verify order book state
          "total" (/ (parse-integer total_volume_int) (expt 10 8)))))
 
 ;;; Talking to the API
@@ -50,31 +50,31 @@
            "bids" (remove 1/100000000 bids :key #'order-amount)))))
 
 ;;; This function is not thread safe!
-;;; It intentionally returns (values) to show that the book gets modified
 (defun apply-depth-message (depth book)
-  (with-json-slots (now type price delta total) depth
-    (if (print (/= delta total))
-        ;; Modify existing order
-        (if (print (zerop total))
-            ;; Delete order
-            (setf (getjso type book)
-                  (remove price (getjso type book) :key #'order-price))
-            ;; Adjust order volume
-            (let ((order (find price (getjso type book) :key #'order-price)))
-              (assert order)
-              ;; (assert (local-time:timestamp> now (order-time order)))
-              ;; (assert (= total (+ delta (order-amount order))))
-              (setf (order-time order) now
-                    (order-amount order) total)))
-        ;; Add new order
-        (let ((test (if (string= type "bids") #'> #'<))
-              (new (%make-order :price price :amount total :time now)))
-          (if (print (funcall test price (order-price (first (getjso type book)))))
-              ;; Add new "best" order
-              (push new (getjso type book))
-              ;; Add order at appropriate place in list
-              (do* ((tail (getjso type book) (cdr tail)))
-                   ((or (funcall test price (order-price (car tail)))
-                        (null (cdr tail)))
-                    (push new (cdr tail))))))))
-  (values))
+  (with-json-slots (now type price total) depth
+    (assert (member type '("asks" "bids") :test #'string=))
+    (if (print (zerop total))
+        ;; Delete order
+        ;; FIXME: What if there's a more recent order on the books?
+        (setf (getjso type book)
+              (delete price (getjso type book) :key #'order-price :count 1))
+        ;; Add new / adjust existing
+        (let ((order (print (find price (getjso type book) :key #'order-price))))
+          ;; If there's a more recent order on the book, we discard the update
+          (unless (and order (print (local-time:timestamp< now (order-time order))))
+            (if order
+                (setf (order-time order) now
+                      (order-amount order) total)
+                ;; Add new order
+                (let ((test (if (string= type "bids") #'> #'<))
+                      (new (%make-order :price price :amount total :time now)))
+                  (if (funcall test price (order-price (first (getjso type book))))
+                      ;; Add new "best" order
+                      (push new (getjso type book))
+                      ;; Add order at appropriate place in list
+                      (do* ((tail (getjso type book) (cdr tail)))
+                           ((or (funcall test price (order-price (car tail)))
+                                (null (cdr tail)))
+                            (push new (cdr tail))))))))))
+    ;; Return the new order on the books
+    (find (getjso "price" depth) (getjso type book) :key #'order-price)))
