7c8d049b023bb4bc13b0a21f951a7311ed30e646
Adlai Chandrasekhar <munchking@gmail.com>
2014-02-23 17:09:45 +0200

    damnit

--- book.lisp
+++ book.lisp
@@ -8,12 +8,16 @@
 
 (defun make-depth-channel (&optional (output (make-instance 'chanl:unbounded-channel)))
   (chanl:pexec ()
-    (let ((glue (external-program:process-output-stream (external-program:start "node" '("-e" "(function(gox){gox.setup('pubnub');gox.connect('usd');gox.subscribe('depth');gox.on('depth',console.log)})(require('mtgox-orderbook'))") :output :stream))))
-      ;; (read-line glue)                  ; connected to: wss://websocket.mtgox.com
-      ;; (read-line glue)                  ; subscribing to channel: depth.BTCUSD
-      (loop (chanl:send output (parse-depth-message (st-json:read-json glue)) :blockp nil))))
-  ;; Block until we've started receiving depth messages
-  (loop (unless (chanl:recv-blocks-p output) (return)))
+    (let ((glue (external-program:process-output-stream (external-program:start "node" '("-e" "require('pubnub').init({subscribe_key:'sub-c-50d56e1e-2fd9-11e3-a041-02ee2ddab7fe',ssl:true}).subscribe({channel:'24e67e0d-1cad-4cc0-9e7a-f8523ef460fe',message:function(m){console.log(m)},connect:function(){console.log('connected to depth')}})") :output :stream))))
+      (chanl:send output (read-line glue))
+      (loop (chanl:send output (parse-depth-message (st-json:read-json glue)) :blockp t))))
+  output)
+
+(defun make-trade-channel (&optional (output (make-instance 'chanl:unbounded-channel)))
+  (chanl:pexec ()
+    (let ((glue (external-program:process-output-stream (external-program:start "node" '("-e" "require('pubnub').init({subscribe_key:'sub-c-50d56e1e-2fd9-11e3-a041-02ee2ddab7fe',ssl:true}).subscribe({channel:'dbf1dee9-4f2e-4a08-8cb7-748919a71b21',message:function(m){console.log(m)},connect:function(){console.log('connected to trade')}})") :output :stream))))
+      (chanl:send output (read-line glue))
+      (loop (chanl:send output (parse-depth-message (st-json:read-json glue)) :blockp t))))
   output)
 
 (defun make-order-book-channel (&key
@@ -23,10 +27,15 @@
                                   (full nil))
   (values output
           (chanl:pexec (:name name)
-            ;; make-depth-channel waits until the channel contains messages
-            (let* ((depth (make-depth-channel (make-instance 'chanl:unbounded-channel)))
-                   ;; we'll update this book every time orders are requested
-                   (book (request connection (make-instance 'book :full full))))
+            (let ((depth (make-depth-channel))
+                  (trade (make-trade-channel)))
+              ;; we have to buffer both streams before requesting the book
+              (loop
+                 (when (and (not (chanl:recv-blocks-p depth))
+                            (not (chanl:recv-blocks-p trade)))
+                   (return)))
+              ;; we'll update this book every time orders are requested
+              (book (request connection (make-instance 'book :full full)))
               ;; updater loop
               (loop
                  (chanl:select
@@ -38,13 +47,25 @@
                     (setf book (apply-depth-message depth book)))))))))
 
 (defun parse-depth-message (raw-jso)
-  (with-json-slots (type_str price_int volume_int total_volume_int now)
-      raw-jso ;; (getjso "depth" raw-jso)
-    (jso "now" (goxstamp (parse-integer now))
-         "type" (concatenate 'string type_str "s")
-         "price" (parse-integer price_int)
-         "delta" (parse-integer volume_int) ; TODO: Verify order book state
-         "total" (parse-integer total_volume_int))))
+  (if (string= "trade" (getjso "private" raw-jso))
+      (with-json-slots (trade_type price_int amount_int properties tid primary)
+          (getjso "trade" raw-jso)
+        (format t "~&~D ~:[BUY~;SELL~] ~12D @ ~D ~A ~A"
+                (goxstamp (parse-integer tid))
+                (string= "ask" trade_type) amount_int price_int
+                properties primary)
+        (jso "now" (goxstamp (parse-integer tid))
+             "type" (concatenate 'string trade_type "s")
+             "price" (parse-integer price_int)
+             "delta" (- (parse-integer amount_int))
+             "total" (when (string= properties "market") 0)))
+      (with-json-slots (type_str price_int volume_int total_volume_int now)
+          (getjso "depth" raw-jso)
+        (jso "now" (goxstamp (parse-integer now))
+             "type" (concatenate 'string type_str "s")
+             "price" (parse-integer price_int)
+             "delta" (parse-integer volume_int) ; TODO: Verify order book state
+             "total" (parse-integer total_volume_int)))))
 
 ;;; Talking to the API
 
@@ -69,17 +90,17 @@
           (bids (reduce (lambda (bids order) (cons (make-order order) bids))
                         bids :initial-value nil)))
       (jso "now" now
-           "asks" (remove 1/100000000 asks :key #'order-amount)
-           "bids" (remove 1/100000000 bids :key #'order-amount)))))
+           "asks" (remove (expt 10 6) asks :key #'order-amount :test #'>)
+           "bids" (remove (expt 10 6) bids :key #'order-amount :test #'>)))))
 
 ;;; This function is now thread safe!
 (defun apply-depth-message (depth book)
   ;; First, pick apart the depth message
-  (with-json-slots (now type price total) depth
+  (with-json-slots (now type price total delta) depth
     ;; Basic sanity
     (assert (member type '("asks" "bids") :test #'string=))
     ;; ;; Debugging
-    ;; (if (zerop total)
+    ;; (if (or (null total) (zerop total))
     ;;     (format t "~&~D REM ~:[ASK~;BID~] @ ~10D"
     ;;             now (string= type "bids") price)
     ;;     (format t "~&~D ~:[ASK~;BID~] ~12D @ ~10D"
@@ -90,35 +111,33 @@
           (new-book  (jso "now"  (getjso "now"  book)
                           "asks" (getjso "asks" book)
                           "bids" (getjso "bids" book)))
-          ;; Nothing special here, just the order we'll be inserting later
-          (new-order (%make-order :price price :amount total :time now))
           ;; Lets us know when we find the right spot for the new order
-          (test      (if (string= type "bids") #'> #'<)))
-      (labels (
-               ;; This helper function is used to express the following flow
-               ;; recursively, instead of a terrible DO (or worse, LOOP)
-               ;; TODO: use tail calls
-               (updated-orders (remaining-orders &aux (first (first remaining-orders)))
-                 (cond
-                   ;; Did we reach the spot where this order belongs?
-                   ((or (not remaining-orders) (funcall test price (order-price first)))
-                    ;; Put it before them and return
-                    (cons new-order remaining-orders))
-                   ;; Does the depth change affect the first remaining order?
-                   ((= price (order-price first))
-                    ;; Is the data on the books more recent than this new order?
-                    (if (local-time:timestamp< now (order-time first))
-                        ;; If so, return the book unchanged
-                        (return-from apply-depth-message book)
-                        ;; If not, put the new data in place of the old
-                        (cons new-order (rest remaining-orders))))
-                   ;; Continue on down the order list
-                   (t (cons first (updated-orders (rest remaining-orders)))))))
-        (setf (getjso type new-book)
-              (if (< total (expt 10 6)) ; anything less is dust or deletion
-                  (remove price (getjso type book) :key 'order-price :count 1)
-                  (updated-orders (getjso type book))))
-        new-book))))
+          (before    (if (string= type "bids") #'> #'<)))
+      (flet ((new-order (total) (%make-order :price price :amount total :time now)))
+        (labels (
+                 ;; This helper function is used to express the following flow
+                 ;; recursively, instead of a terrible DO (or worse, LOOP)
+                 ;; TODO: use tail calls
+                 (updated-orders (remaining-orders &aux (first (first remaining-orders)))
+                   (cond
+                     ;; Did we reach the spot where this order belongs?
+                     ((or (not remaining-orders) (funcall before price (order-price first)))
+                      ;; Put it before them and return
+                      (cons (new-order total) remaining-orders))
+                     ;; Does the depth change affect the first remaining order?
+                     ((= price (order-price first))
+                      ;; Build the appropriate modification
+                      (cons (new-order (if (not total)
+                                           (+ delta (order-amount first))
+                                           total))
+                            (rest remaining-orders)))
+                     ;; Continue on down the order list
+                     (t (cons first (updated-orders (rest remaining-orders)))))))
+          (setf (getjso type new-book)
+                (if (or (not total) (< total (expt 10 6))) ; dust or deletion
+                    (remove price (getjso type book) :key 'order-price :count 1)
+                    (updated-orders (getjso type book))))
+          new-book)))))
 
 (defun spread (book)
   (- (order-price (first (getjso "asks" book)))
