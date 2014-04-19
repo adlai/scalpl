50ef8d8774c383002200eb99b40e8f83143d3183
Adlai Chandrasekhar <munchking@gmail.com>
2014-04-19 13:45:19 +0300

    SUCH WOW

--- qd.lisp
+++ qd.lisp
@@ -7,6 +7,15 @@
 
 (defvar *auth*)
 
+(defun auth-request (path &optional options)
+  (when *auth* (post-request path (car *auth*) (cdr *auth*) options)))
+
+(defun get-markets (&aux (pairs (get-request "AssetPairs")))
+  (mapjso (lambda (name data)
+            (setf (getjso "name" data) name))
+          pairs)
+  pairs)
+
 (defun get-book (pair &optional count)
   (with-json-slots (bids asks)
       (getjso pair
@@ -22,36 +31,99 @@
             (bids (mapcar #'parse bids)))
         (values asks bids)))))
 
+(defun open-orders ()
+  (mapjso* (lambda (id order) (setf (getjso "id" order) id))
+           (getjso "open" (auth-request "OpenOrders"))))
+
+(defun cancel-order (id)
+  (auth-request "CancelOrder" `(("txid" . ,id))))
+
+(defun cancel-pair-orders (pair)
+  (mapjso (lambda (id order)
+            (when (string= pair (getjso* "descr.pair" order))
+              (format t "~&REM ~A~%" (getjso* "descr.order" order))
+              (cancel-order id)))
+          (open-orders)))
+
+(defun post-limit (type pair price volume &optional options)
+  (multiple-value-bind (info errors)
+      (auth-request "AddOrder"
+                    `(("ordertype" . "limit")
+                      ("type" . ,type)
+                      ("pair" . ,pair)
+                      ("volume" . ,(princ-to-string volume))
+                      ("price" . ,(princ-to-string price))
+                      ,@(when options `(("oflags" . ,options)))
+                      ;; ("validate" . "true")
+                      ))
+    (if errors
+        (dolist (message errors)
+          (if (and (search "volume" message)
+                   (not (search "viqc" options)))
+              (post-limit type pair price (* volume price)
+                          (apply #'concatenate 'string "viqc"
+                                 (when options '("," options))))
+              (format t "~&~A~%" message)))
+        (progn
+          (format t "~&ADD ~A~%" (getjso* "descr.order" info))
+          (getjso "descr.txid" info)))))
+
+(defparameter *last-candle-id* 0)
+
+(defparameter *max-seen-vol* 0)
+
+(defun track-vol (pair interval)
+  (with-json-slots (last (candles pair))
+      (get-request "OHLC"
+                   `(("pair" . ,pair)
+                     ("interval" . ,(princ-to-string interval))))
+    (setf *last-candle-id* last
+          *max-seen-vol* (reduce 'max
+                                 (mapcar 'read-from-string
+                                         (map 'list 'seventh candles)))))
+  (format t "~&~A highest ~D minute volume - ~F"
+          (local-time:unix-to-timestamp *last-candle-id*)
+          interval *max-seen-vol*))
+
+(defparameter *max-seen-vol*
+  (or (and (boundp '*max-seen-vol*) *max-seen-vol*)
+      (track-vol "XXBTXXDG" 5)))
+
 (defun dumbot-oneside (book resilience funds &aux (acc 0))
   ;; calculate cumulative depths
-  (do ((cur book (cdr cur)))
+  (do ((cur book (cdr cur))
+       (n 0 (1+ n)))
       ((> acc resilience)
-       ;; drop the tail
-       (rplaca (rplacd cur nil) nil))
+       (setf book (subseq book 0 n)))
     ;; modifies the book itself
     (push (incf acc (cdar cur))
           (car cur)))
   (mapcar (lambda (order)
             (cons (* funds (/ (cddr order) acc))
                   (cadr order)))
-           (print (butlast book))))
+          book))
 
-(defun open-orders ()
-  (when *auth*
-    (mapcar-jso (lambda (id order)
-                  (setf (getjso "id" order) id)
-                  order)
-                (getjso "open"
-                        (post-request "OpenOrders"
-                                      (car *auth*)
-                                      (cdr *auth*))))))
-
-(defun cancel-order (id)
-  (when *auth*
-    (post-request "CancelOrder"
-                  (car *auth*)
-                  (cdr *auth*)
-                  `(("txid" . ,id)))))
-
-(defun place-order (&key type price volume expiration ref validate)
-  (post-request "AddOrder"))
+(defun %round (fund-factor resilience-factor)
+  ;; Track our resilience target
+  (track-vol "XXBTXXDG" 5)
+  ;; Get our balances
+  (let ((balances (auth-request "Balance"))
+        (resilience (* resilience-factor *max-seen-vol*)))
+    (flet ((factor-funds (symbol)
+             (* fund-factor (read-from-string (getjso symbol balances)))))
+      (let ((btc (factor-funds "XXBT"))
+            (doge (factor-funds "XXDG")))
+        ;; report funding
+        (format t "~&Risking ~9,1F doge and ~6,3F btc~%" doge btc)
+        ;; Now run that algorithm thingy
+        ;; First, remove our liquidity, so we don't watch ourselves
+        (cancel-pair-orders "XBTXDG")
+        ;; Next, get the current order book status
+        (multiple-value-bind (asks bids) (get-book "XXBTXXDG")
+          ;; Finally, inject our liquidity
+          (mapc (lambda (info)
+                  (post-limit "buy" "XXBTXXDG" (+ (cdr info) 0.1) (car info) "viqc"))
+                (dumbot-oneside bids resilience doge))
+          (mapc (lambda (info)
+                  (post-limit "sell" "XXBTXXDG" (- (cdr info) 0.1) (car info)))
+                (dumbot-oneside asks resilience btc)))))))
