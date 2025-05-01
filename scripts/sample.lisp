5febf184e1ad96b3e6270d854c462e369433ad70
Adlai Chandrasekhar <adlai.chandrasekhar@gmail.com>
2025-05-01 14:15:03 +0300

    helpful?

--- scripts/sample.lisp
+++ scripts/sample.lisp
@@ -15,6 +15,13 @@
   (make-instance 'supplicant :order-slots 13 :gate *gate*
 		             :market (find-market "XBTUSD" :kraken)))
 
+;;; MAIS POURQUOI PARLEZ-NOUS SEULEMENT BON MOTS POOR LAGNIAPPE
+(warn "Soon, there will be much recurrence of your chosen word;")
+(defvar user-chosen-word)
+(princ " ... YOUR CHOSEN WORD MUST BE LIKE OLD PASSWORD, OK!?")
+(print "Please, without spaces, preferably alphanumerics only:")
+(defparameter user-chosen-word (read-line))
+
 (define-maker *wbtcr* :supplicant *supplicant* :targeting 1)
 
 ;;; use amend orders when possible to improve API efficiency
