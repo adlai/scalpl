4f200ab9fd3ff6c3e032db4bcf500150ddbe92c5
Adlai Chandrasekhar <adlai.chandrasekhar@gmail.com>
2025-09-16 16:59:28 +0300

    HELP! I NEED YOUR VOICES...

---
+++ venues/lnm_shop.lisp
@@ -0,0 +1,12 @@
+(warn "Please help me lobby that they publish an orderbook!")
+
+;; (defpackage #:scalpl.lnmarkets
+;;   (:nicknames #:lnmarkets) (:export #:*lnmarkets* #:lnmarkets-gate)
+;;   (:use #:cl #:base64 #:chanl #:anaphora #:local-time #:scalpl.util
+;;         #:scalpl.actor #:scalpl.net #:scalpl.exchange #:scalpl.qd))
+
+;; (in-package #:scalpl.lnmarkets)
+
+;; ;;; General Parameters
+;; (defparameter *base-domain* "api.lnmarkets.com")
+;; (defparameter *base-url* (format () "https://~A" *base-domain*))
